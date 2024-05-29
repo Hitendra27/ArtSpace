9de3cdc93169e40b4d3ef3f1a79329782a479939
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2024-05-29 01:09:17 +0100

    feat: implemented Previous Button

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -5,21 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -29,12 +24,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.AbsoluteAlignment
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.BlendMode.Companion.Color
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -88,9 +80,9 @@ fun ArtSpaceApp(modifier: Modifier = Modifier) {
         mutableStateOf(R.string.art_1_year)
     }
 
-    var imageResource by remember {
-        mutableStateOf(currentArtwork)
-    }
+//    var imageResource by remember {
+//        mutableStateOf(currentArtwork)
+//    }
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -115,7 +107,78 @@ fun ArtSpaceApp(modifier: Modifier = Modifier) {
         ){
             // Previous Button
             Button(
-                onClick = { },
+                onClick = {
+                          when (currentArtwork) {
+                              ninthArtwork -> {
+                                  currentArtwork = tenthArtwork
+                                  artName = R.string.art_10_name
+                                  artist = R.string.art_10_artist
+                                  artYear = R.string.art_10_year
+                              }
+                              eightArtwork -> {
+                                  currentArtwork = ninthArtwork
+                                  artName = R.string.art_9_name
+                                  artist = R.string.art_9_artist
+                                  artYear = R.string.art_9_year
+                              }
+
+                              seventhArtwork -> {
+                                  currentArtwork = eightArtwork
+                                  artName = R.string.art_8_name
+                                  artist = R.string.art_8_artist
+                                  artYear = R.string.art_8_year
+                              }
+
+                              sixthArtwork -> {
+                                  currentArtwork = seventhArtwork
+                                  artName = R.string.art_7_name
+                                  artist = R.string.art_7_artist
+                                  artYear = R.string.art_7_year
+                              }
+
+                              fifthArtwork -> {
+                                  currentArtwork = sixthArtwork
+                                  artName = R.string.art_6_name
+                                  artist = R.string.art_6_artist
+                                  artYear = R.string.art_6_year
+                              }
+
+                              fourthArtwork -> {
+                                  currentArtwork = fifthArtwork
+                                  artName = R.string.art_5_name
+                                  artist = R.string.art_5_artist
+                                  artYear = R.string.art_5_year
+                              }
+
+                              thirdArtwork -> {
+                                  currentArtwork = fourthArtwork
+                                  artName = R.string.art_4_name
+                                  artist = R.string.art_4_artist
+                                  artYear = R.string.art_4_year
+                              }
+
+                              secondArtwork-> {
+                                  currentArtwork = thirdArtwork
+                                  artName = R.string.art_3_name
+                                  artist = R.string.art_3_artist
+                                  artYear = R.string.art_3_year
+                              }
+
+                              fifthArtwork -> {
+                                  currentArtwork = secondArtwork
+                                  artName = R.string.art_2_name
+                                  artist = R.string.art_2_artist
+                                  artYear = R.string.art_2_year
+                              }
+
+                              else -> {
+                                  currentArtwork = firstArtwork
+                                  artName = R.string.art_1_name
+                                  artist = R.string.art_1_artist
+                                  artYear = R.string.art_1_year
+                              }
+                          }
+                },
                 modifier = Modifier
                     .height(35.dp)
                     .width(150.dp)
